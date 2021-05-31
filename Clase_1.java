c3f09f6290501a4c96e3b8dc8d147a9f89a8ead8
AdrianCode90 <51031404+AdrianCode90@users.noreply.github.com>
2021-05-31 14:24:07 -0300

    Update Clase_1.java

--- Clase_1.java
+++ Clase_1.java
@@ -8,6 +8,8 @@ public class Clase_1 {
     public static void main(String[] args) {
         
         System.out.println("Me llamo pepito");
+        System.out.println("Me llamo adrian");
+        
     }
     
 }
